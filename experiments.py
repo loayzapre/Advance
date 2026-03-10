fe13084540f508c850c961b84e6312c1b54e0462
loayzapre <s252608@dtu.dk>
2026-03-10 12:36:06 +0100

    just in case, saved changes

--- experiments.py
+++ experiments.py
@@ -131,7 +131,7 @@ def main():
     
 
     priors = ["gaussian", "mog", "flow"] # gaussian, mog, flow
-    seeds = [0, 1, 2]
+    seeds = [0, 1, 2, 3, 4] # Multiple seeds for better estimates of performance
     M = 2 # Latent dimension, M = 2 for visualization
     epochs = 50
 
